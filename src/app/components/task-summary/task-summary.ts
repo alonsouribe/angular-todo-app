799ad844dc752efd4be56d15854d9574db976d0f
Alonso Uribe <alonsouribe09@gmail.com>
2026-05-05 10:45:01 -0700

    remove components

--- src/app/components/task-summary/task-summary.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-task-summary',
-  imports: [],
-  templateUrl: './task-summary.html',
-  styleUrl: './task-summary.scss',
-})
-export class TaskSummary {}
